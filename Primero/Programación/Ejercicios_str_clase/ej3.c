91bb2ca8ce82a93256eef5e94b39d63ee6cc407b
Antonio Delgado Luque <tonyluque2006@gmail.com>
2024-10-29 14:31:40 +0100

    clase 29/10/2024

--- Primero/Programación/Ejercicios_str_clase/ej3.c
+++ Primero/Programación/Ejercicios_str_clase/ej3.c
@@ -11,14 +11,15 @@
  */
 
 int main() {
-	char str1[4];
-	char str2[4];
-	scanf("%s",str1);
-	scanf("%s",str2);
+	char str2[8];
+	char str1[8];
+	char str3[8];
+	scanf(" %s",str1);
+	scanf(" %s",str2);
 	if (str1==str2){
 		printf("str1==str2\n");
 	}else if (str1!=str2){
-		printf("str1!=str2\n");
+		printf("str1!=str2\n");//Siempre van a ser distintas incluso si tienen la misma palabra ya que lo que se compara es la dirección de memoria de inicio de la cadena la cual no es igual
 	}else{
 		printf("error1\n");
 	}
@@ -29,14 +30,16 @@ int main() {
 	}else{
 		printf("error2\n");
 	}
-	for (int i=0;i<=50;i++){
+	for (int i=0;i<8;i++){
 		if (str1[i]==str2[i]){
 			printf("%c",str1[i]);
-		}else{break;
+		}else{
+			break;
 		}
 	}
 	printf("\n");
-
+	str3=str1;
+	printf("%s\n",str3);
 
 	return 0;
 }
