f316f5944cb2f5362b28dea1c1dc620ab24bce70
Antonio Delgado Luque <tonyluque2006@gmail.com>
2024-11-15 14:24:18 +0100

    fin de clase 15/11/24

--- Primero/Programación/Punteros/ejercicio4_punteros.c
+++ Primero/Programación/Punteros/ejercicio4_punteros.c
@@ -9,21 +9,19 @@
  *
  *	Título:
  */
-void intercambio();
-void cambio(int *n1,int *n2){
 
-	intercambio(&n1,&n2,*n2,*n1);
-}
+void intercambio(int *a,int *b){
+	int var1=*a;
+	int var2=*b;
 
-void intercambio(int *a,int *b,int c,int d){
-
-	*a=c;
-	*b=d;
+	*a=var2;
+	*b=var1;
 }
 int main(){
   int num1=10,num2=20;
   printf("num1 %d, num2 %d\n",num1,num2);
-  cambio(&num1,&num2);
+  intercambio(&num1,&num2);
   printf("num1 %d, num2 %d\n",num1,num2);
   return 0;
-}
+  
+ }
