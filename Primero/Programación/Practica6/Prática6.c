33e94c448c229dca7061fdc7809caaa684eb69ad
Antonio Delgado Luque <tonyluque2006@gmail.com>
2024-12-10 14:32:28 +0100

    Fin de clase 10/12/24

--- Primero/Programación/Practica6/Prática6.c
+++ Primero/Programación/Practica6/Prática6.c
@@ -7,9 +7,9 @@
  *	
  *	Fecha inicio:27/11/2024
  *
- *	Título:Práctica 6: Gestión de biblioteca
+ *	Título: Práctica 6: Gestión de biblioteca
  */
-/*DEFINE*/
+/*DEFINES*/
 #define MAX_TITULO 80
 #define MAX_AUTOR 50
 #define MAX_LIBROS 40
@@ -34,104 +34,142 @@ typedef struct{
 	int stock;
 }Libro;
 
-/*FUNCIÓN PARA MOSTRAR TODOS LOS LIBROS*/
-void mostrarlibros(Libro * datos){
-for (int i = 0; i < MAX_LIBROS; ++i){
-printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n",(datos+i)->ID, (datos+i)->titulo, (datos+i)->autor, (datos+i)->precio, (datos+i)->genero, (datos+i)->stock);
+
+//Como norma general paso por referencia ya que es más eficiente en términos de memoria aunque se podría pasar por valor los parámetros en todas las funciones menos la función "aumentarstock".
+//Uso const ya que no quiero que se modifique por error ninguno de los Parámetros que recibe por referencia. Esto lo hago en todas menos en la función "aumentarstock".
+
+/*FUNCIÓN GENERAL PARA IMPRIMIR LOS DATOS DE LOS LIBROS*/
+void imprimirlibro( const Libro * const datos){ 
+	printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n\n",(datos)->ID, (datos)->titulo, (datos)->autor, (datos)->precio, (datos)->genero, (datos)->stock);
+																								//Utilizo flechas cuando necesito llamar al contenido de partes del struct. 
 };
+/*FUNCIÓN PARA MOSTRAR TODOS LOS LIBROS*/
+
+void mostrarlibros(const Libro * const datos){
+	for (int i = 0; i < MAX_LIBROS; ++i){//El bucle recorre todo el Array de libros y los imprime.
+		imprimirlibro(&datos[i]);
+	};
 };
 
 /*FUNCIÓN PARA MOSTRAR EL LIBRO CORRESPONDIENTE CON EL ID INTRODUCIDO*/
-void mostrarlibroid(int ID_Libro,Libro * datos){
-	if ((ID_Libro<0)|(ID_Libro>=MAX_LIBROS))
-	{
-		printf("Error\n");
-	}else{
-	printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n",(datos+ID_Libro)->ID, (datos+ID_Libro)->titulo, (datos+ID_Libro)->autor, (datos+ID_Libro)->precio, (datos+ID_Libro)->genero, (datos+ID_Libro)->stock);
-	};
-	};
-/*FUNCIÓN PARA AUMENTAR EL STOCK DE UN LIBRO*/
-void aumentarstock(int ID_Libro,int AumentarStock, Libro * datos){
-printf("Stock previo: %d \n",(datos+ID_Libro)->stock);
 
-datos[ID_Libro].stock+=AumentarStock;
-printf("Stock actualizado: %d \n",(datos+ID_Libro)->stock);
+//En el caso de ID_Libro no me preocupa ya que es una variable que contiene un entero así que la paso por valor en ver de referencia para evitar posibles problemas
+void mostrarlibroid(int ID_Libro,const Libro * const datos){
+	if ((ID_Libro<0)|(ID_Libro>=MAX_LIBROS))//Compruebo que el id a buscar está dentro del máximo de libros
+	{
+		printf("Error\n");//si no lo está o se escribe algun carácter no numérico muestra error
+	}else{
+		imprimirlibro(&datos[ID_Libro]);//en este caso se imprime el libro correspondiente al ID introducido ya que no hay bucle como en el resto de los casos
+	};
+};
+/*FUNCIÓN PARA AUMENTAR EL STOCK DE UN LIBRO*/
+
+//En esta función es necesario pasar por referencia los datos esto debido a que si lo paso por valor la modificación no se aplica al array original. 
+//También es necesario no usar const ya que vamos a modificar el stock del libro. 
+//En el caso de ID_Libro y AumentarStock no me preocupa ya que es una variable que contiene un entero así que la paso por valor en ver de referencia para evitar posibles problemas
+void aumentarstock(int ID_Libro,int AumentarStock,  Libro * const datos){
+	if ((ID_Libro<0)|(ID_Libro>=MAX_LIBROS)){//Compruebo que el id a buscar NO está dentro del máximo de libros
+
+		printf("Error\n");//Si no lo está o se escribe algun carácter no numérico muestra error
+	}else{
+
+		printf("Stock previo: %d \n",(datos+ID_Libro)->stock);
+
+	datos[ID_Libro].stock+=AumentarStock;//Se aumenta el stock del libro correspondiente al id introducido por la cantidad introducida
 	
+	printf("Stock actualizado: %d \n",(datos+ID_Libro)->stock);
+
+};
 };
 
 /*MOSTRAR TODOS LOS LIBROS DE UNA CATEGORÍA*/
-void mostrarlibroscategoria(char Categoria_Buscar,Libro * datos){
-switch (Categoria_Buscar){
+void mostrarlibroscategoria(int Categoria_Buscar,const Libro * const datos){
+	/*Cada caso está asignado a una categoría definida en el enum. 
+	Como los datos del enum aunque con nombre propio (Ej. FICCION) realmente son numeros enteros de ahí que los "case" sean numeros enteros*/
 
-case 0:
-for (int i = 0; i < MAX_LIBROS; ++i){
-if (Categoria_Buscar==datos[i].genero){
-	printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n",(datos+i)->ID, (datos+i)->titulo, (datos+i)->autor, (datos+i)->precio, (datos+i)->genero, (datos+i)->stock);	
-};
-};
-break;
+	switch (Categoria_Buscar){
 
-case 1:
-for (int i = 0; i < MAX_LIBROS; ++i){
-if (Categoria_Buscar==datos[i].genero){
-	printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n",(datos+i)->ID, (datos+i)->titulo, (datos+i)->autor, (datos+i)->precio, (datos+i)->genero, (datos+i)->stock);	
-};
-};
-break;
+	case 0:
+		for (int i = 0; i < MAX_LIBROS; ++i){
+			if (Categoria_Buscar==datos[i].genero){//Aquí en vez de usar flechas uso punto"datos[i].genero" ya que es mas entendible que la alternativa con flechas "(datos+i)->genero".Esto lo aplico en todos los bucles
+				imprimirlibro(&datos[i]);
+			};
+		};
+		break;
 
-case 2:
-for (int i = 0; i < MAX_LIBROS; ++i){
-if (Categoria_Buscar==datos[i].genero){
-	printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n",(datos+i)->ID, (datos+i)->titulo, (datos+i)->autor, (datos+i)->precio, (datos+i)->genero, (datos+i)->stock);	
-};
-};
-break;
+	case 1:
+		for (int i = 0; i < MAX_LIBROS; ++i){
+			if (Categoria_Buscar==datos[i].genero){
+				imprimirlibro(&datos[i]);
+			};
+		};
+		break;
 
-case 3:
-for (int i = 0; i < MAX_LIBROS; ++i){
-if (Categoria_Buscar==datos[i].genero){
-	printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n",(datos+i)->ID, (datos+i)->titulo, (datos+i)->autor, (datos+i)->precio, (datos+i)->genero, (datos+i)->stock);	
-};
-};
-break;
+	case 2:
+		for (int i = 0; i < MAX_LIBROS; ++i){
+			if (Categoria_Buscar==datos[i].genero){
+				imprimirlibro(&datos[i]);
+			};
+		};
+		break;
 
-case 4:
-for (int i = 0; i < MAX_LIBROS; ++i){
-if (Categoria_Buscar==datos[i].genero){
-	printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n",(datos+i)->ID, (datos+i)->titulo, (datos+i)->autor, (datos+i)->precio, (datos+i)->genero, (datos+i)->stock);	
-};
-};
-break;
+	case 3:
+		for (int i = 0; i < MAX_LIBROS; ++i){
+			if (Categoria_Buscar==datos[i].genero){
+				imprimirlibro(&datos[i]);
+			};
+		};
+		break;
 
-default:
-printf("Error\n");
-};
+	case 4:
+		for (int i = 0; i < MAX_LIBROS; ++i){
+			if (Categoria_Buscar==datos[i].genero){
+				imprimirlibro(&datos[i]);
+			};
+		};
+		break;
+
+	default:
+		printf("Error\n"); //si no se introdujo un valor válido muestra error
+	};
 };
 
 /*MOSTRAR TODOS LOS LIBROS DE UN AUTOR*/
-void mostrarlibrosautor(char * Autor_Buscar,Libro * datos){
-printf("simón %s\n",Autor_Buscar);	
-for (int i = 0; i < MAX_LIBROS; ++i){
-if(strcmp(Autor_Buscar,datos[i].autor)==0){
-printf("ID: %d \nTítulo: %s \nAutor/es:  %s \nPrecio: %f \nGénero: %d \nStock: %d \n",(datos+i)->ID, (datos+i)->titulo, (datos+i)->autor, (datos+i)->precio, (datos+i)->genero, (datos+i)->stock);
-};
-};
 
-printf("Fin\n");
-};
+void mostrarlibrosautor(const char * const Autor_Buscar,const Libro * const datos){
+	/*Se compara el autor introducido con el string autor de cada libro, Se empieza con una comparación normal. Si son iguales imprime los datos del libro.
+	 En caso contrario se vuelve a comparar pero esta vez el string del autor del libro que toque inicia un caracter después que en el anterior. 
+	 Este ciclo se repite hasta que los strings coinciden o acaba el string*/
+	int AutorOK;
+	for (int i = 0; i < MAX_LIBROS; ++i){
+		AutorOK=0;
+		for (int j = 0; j < (MAX_AUTOR-(strlen(Autor_Buscar)-1)); ++j){
 
+			if (strncmp(Autor_Buscar,datos[i].autor+j,(strlen(Autor_Buscar)-1))==0){ 
+				AutorOK=1;
+				break;
+			};
+		};
+		if (AutorOK==1){		
+				imprimirlibro(&datos[i]);
+
+		};
+
+	
+	};
+};
 
 
 void main() {
 /*DEFINICIÓN DE VARIABLES*/
-int accion;
-int ID_Libro;
-int AumentarStock;
-int Categoria_Buscar;
-char Autor_Buscar[MAX_AUTOR];
+	int accion;
+	int ID_Libro;
+	int AumentarStock;
+	int Categoria_Buscar;
+	char Autor_Buscar[MAX_AUTOR];
 
 	Libro Libros[MAX_LIBROS] = {
-		{1, "To Kill a Mockingbird", "Harper", 15.99, FICCION, 10},
+		{1, "To Kill a Mockingbird", "Harper Lee", 15.99, FICCION, 10},
 		{2, "1984", "George Orwell", 12.49, FICCION, 5},
 		{3, "The Great Gatsby", "F. Scott Fitzgerald", 10.99, FICCION, 8},
 		{4, "Moby Dick", "Herman Melville", 18.99, FICCION, 12},
@@ -173,53 +211,53 @@ char Autor_Buscar[MAX_AUTOR];
 		{40, "Thus Spoke Zarathustra", "Friedrich Nietzsche", 14.99, ENSAYO, 10}
 	}; 
 
-	do{
-	printf("Elige la acción: \n 1.Mostrar todos los libros. \n 2.Mostrar un libro por ID \n 3.Aumentar stock de un libro \n 4.Mostrar todos los libros de una categoría \n 5.Mostrar los libros de un autor \n 6.Salir\n");
+	do{//Switch case en un bucle do while para que el menú se ejecute después de cada acción hasta que el usuario seleccione 6 (Salir) 
+		printf("Elige la acción: \n 1.Mostrar todos los libros. \n 2.Mostrar un libro por ID \n 3.Aumentar stock de un libro \n 4.Mostrar todos los libros de una categoría \n 5.Mostrar los libros de un autor \n 6.Salir\n");
 
-	scanf("%d",&accion);
-	
-	switch(accion){
+		scanf("%d",&accion);
 
-	case 1:
-		mostrarlibros(Libros);
-		break;
+		switch(accion){
 
-	case 2:
+		case 1:
+			mostrarlibros(Libros);
+			break;
 
-		printf("Introduce el ID a buscar: ");
-		scanf("%d",&ID_Libro);
-		mostrarlibroid(ID_Libro-1,Libros);
-		break;
+		case 2:
 
-	case 3:
+			printf("Introduce el ID a buscar: ");
+			scanf("%d",&ID_Libro);
+			mostrarlibroid(ID_Libro-1,Libros);//Le resto 1 a ID_Libro ya que el array empieza en 0 pero el primer ID es el 1
+			break;
 
-		printf("Introduce el ID a aumentar el stock: ");
-		scanf("%d",&ID_Libro);
-		printf("Introduce la cantidad a añadir al stock: ");
-		scanf("%d",&AumentarStock);
-		aumentarstock(ID_Libro-1,AumentarStock,Libros);
-		break;
+		case 3:
 
-	case 4:
+			printf("Introduce el ID a aumentar el stock: ");
+			scanf("%d",&ID_Libro);
+			printf("Introduce la cantidad a añadir al stock: ");
+			scanf("%d",&AumentarStock);
+			aumentarstock(ID_Libro-1,AumentarStock,Libros);//Le resto 1 a ID_Libro ya que el array empieza en 0 pero el primer ID es el 1
+			break;
 
-		printf("Introduce la categoría a mostrar: ");
-		scanf("%d",&Categoria_Buscar);
-		mostrarlibroscategoria(Categoria_Buscar,Libros);
-		break;
+		case 4:
 
-	case 5:
-		printf("Introduce el nombre del autor a buscar: ");
-		scanf(" ");
-		fgets(Autor_Buscar,MAX_AUTOR*sizeof(char),stdin);
-		mostrarlibrosautor(Autor_Buscar,Libros);
-		break;
+			printf("Introduce la categoría a mostrar:\n 0.FICCION\n 1.NO_FICCION\n 2.POESIA\n 3.TEATRO\n 4.ENSAYO\n ");
+			scanf("%d",&Categoria_Buscar);
+			mostrarlibroscategoria(Categoria_Buscar,Libros);
+			break;
 
-	case 6:
-		accion=6;
-		break;
-	
-	default:
-		printf("Error");
-	}
-}while(accion!=6);
+		case 5:
+			printf("Introduce el nombre del autor a buscar: ");
+			scanf(" ");//Este scanf está aquí para consumir el \n de seleccionar la acción y que funcione el fgets. QUITAR CON CUIDADO
+			fgets(Autor_Buscar,MAX_AUTOR,stdin);//Recoge el nombre del autor con espacios para luego utilizarlo en la función
+			mostrarlibrosautor(Autor_Buscar,Libros);
+			break;
+
+		case 6:
+			accion=6;//sale del programa
+			break;
+
+		default:
+			printf("Error");//si no se introduce un número del 1 al 6 muestra error
+		}
+	}while(accion!=6);
 }
